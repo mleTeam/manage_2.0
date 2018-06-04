a7d133d337e6d208a031df58e81ebb1644efafed
www555706 <www555706@sina.com>
2018-06-04 17:49:36 +0800

    no message

---
+++ pages/home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="home">
+          {{title}}
+    </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      title: '欢迎使用米粒儿金融管理平台'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/users/assets.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="assets">
+        用户资金
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
