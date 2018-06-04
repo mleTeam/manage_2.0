25331d4fbff7a875cbed470f08b2be99f0ad363b
LAPTOP-H7PMPE65\xc <347691082@qq.com>
2018-06-04 18:01:39 +0800

    11
    Signed-off-by: LAPTOP-H7PMPE65\xc <347691082@qq.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,14 +47,14 @@ module.exports = {
     [
       '/v2',
       {
-        target: 'http://csactivity.milibanking.com:9050', // api主机
+        target: 'http://csmanage2.milibanking.com', // api主机
         ws: false
       }
     ]
   ],
   axios: {
     proxy: true,
-    baseURL: 'http://csactivity.milibanking.com'
+    baseURL: 'http://csmanage2.milibanking.com'
   },
 
   // proxy: {
